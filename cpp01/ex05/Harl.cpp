9b3a2a6dd909c9befaf0b8adaa83adb6d45a34a1
bookseal <gichanlee@icloud.com>
2023-11-11 20:48:14 +0900

    ex05

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,39 @@
+#include "Harl.hpp"
+
+void Harl::debug(void)
+{
+	std::cout << "[DEBUG] This is a debug message." << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << "[INFO] This is an info message." << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << "[WARNING] This is a warning message." << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << "[ERROR] This is an error message." << std::endl;
+}
+
+typedef void (Harl::*HarlFunction)();
+
+void Harl::complain(std::string level)
+{
+    HarlFunction functions[] = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+    std::string levels[] = { "debug", "info", "warning", "error" };
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (level == levels[i])
+        {
+            (this->*functions[i])();
+            return;
+        }
+    }
+    std::cout << "[UNKNOWN] This is an unknown message." << std::endl;
+}
