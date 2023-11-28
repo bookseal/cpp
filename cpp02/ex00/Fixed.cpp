016032056c8f5f2f5c370095b9a6bcf200f98d7f
Gichan Lee <gichanlee@icloud.com>
2023-11-28 14:15:09 +0900

    cpp02

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,34 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_fixedPointValue = 0;
+}
+
+Fixed::Fixed(const Fixed &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed &Fixed::operator=(const Fixed &rhs)
+{
+	this->_fixedPointValue = rhs.getRawBits();	
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedPointValue);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_fixedPointValue = raw;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
