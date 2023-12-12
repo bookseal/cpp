e1db47e581950a063f3c12d5e92ab3f0ce2c7842
Gichan Lee <gichanlee@icloud.com>
2023-12-12 23:41:50 +0900

    ex01

---
+++ cpp04/ex01/Dog.cpp
@@ -0,0 +1,34 @@
+#include "Dog.hpp"
+
+Dog::Dog(): Animal()
+{
+	std::cout << "🐶Dog default constructor called" << std::endl;
+	this->setType("Dog");
+	this->brain = new Brain();
+}
+
+Dog::Dog(const Dog& copy)
+{
+	std::cout << "🐶Dog copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Dog::~Dog()
+{
+	std::cout << "🐶Dog destructor called" << std::endl;
+	delete this->brain;
+}
+
+Dog& Dog::operator=(const Dog& op)
+{
+	std::cout << "🐶Dog assignation operator called" << std::endl;
+	if (this == &op)
+		return (*this);
+	this->type = op.type;
+	return (*this);
+}
+
+void Dog::makeSound() const
+{
+	std::cout << "🐶Dog makeSound called" << std::endl;
+}
