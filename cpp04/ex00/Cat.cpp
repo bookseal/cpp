7a01bc99abe9e1ba695ab4ecd5158222579488eb
gichlee <gichlee@student.42seoul.kr>
2023-12-15 20:10:02 +0900

    ex00 added print

--- cpp04/ex00/Cat.cpp
+++ cpp04/ex00/Cat.cpp
@@ -2,24 +2,24 @@
 
 Cat::Cat(): Animal()
 {
-	std::cout << "🐱Cat default constructor called" << std::endl;
+	std::cout << "😻Cat default constructor called" << std::endl;
 	this->type = "Cat";
 }
 
 Cat::Cat(const Cat& copy)
 {
-	std::cout << "🐱Cat copy constructor called" << std::endl;
+	std::cout << "😻Cat copy constructor called" << std::endl;
 	*this = copy;
 }
 
 Cat::~Cat()
 {
-	std::cout << "🐱Cat destructor called" << std::endl;
+	std::cout << "😻Cat destructor called" << std::endl;
 }
 
 Cat& Cat::operator=(const Cat& op)
 {
-	std::cout << "🐱Cat assignation operator called" << std::endl;
+	std::cout << "😻Cat assignation operator called" << std::endl;
 	if (this == &op)
 		return (*this);
 	this->type = op.type;
@@ -28,5 +28,5 @@ Cat& Cat::operator=(const Cat& op)
 
 void Cat::makeSound() const
 {
-	std::cout << "🐱Cat makeSound called" << std::endl;
+	std::cout << "😻Cat makeSound called" << std::endl;
 }
--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -5,43 +5,56 @@
 #include "WrongCat.hpp"
 #include <iostream>
 
+void leaks(void)
+{
+	system("leaks a.out");
+}
+
+void print(std::string str)
+{
+	std::cout << std::endl << std::endl;
+	std::cout << "Testing: " << str;
+	std::cin.get();
+}
+
 int main() {
-	const Animal* meta = new Animal(); 
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	{
+		print("Animal constructors");
+		const Animal* meta = new Animal(); 
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
 
-	std::cout << std::endl;
+		print("Animal getType()");
+		std::cout << j->getType() << " " << std::endl; 
+		std::cout << i->getType() << " " << std::endl; 
+		
+		print("Animal makeSound()");
+		j->makeSound();
+		i->makeSound();
+		meta->makeSound();
 
-	std::cout << j->getType() << " " << std::endl; 
-	std::cout << i->getType() << " " << std::endl; 
-	
-	std::cout << std::endl;
+		print("delete");
+		delete meta;
+		delete j;
+		delete i;
+	}
+	{
+		print("WrongAnimal");
+		const WrongAnimal* wrongMeta = new WrongAnimal();
+		const WrongAnimal* wrongJ = new WrongCat();
 
-	j->makeSound();
-	i->makeSound();
-    meta->makeSound();
+		std::cout << std::endl;
 
-	std::cout << std::endl;
+		std::cout << wrongJ->getType() << " " << std::endl;	
+		wrongJ->makeSound();
+		wrongMeta->makeSound();
 
-	delete meta;
-	delete j;
-	delete i;
+		std::cout << std::endl;
 
-	std::cout << std::endl;
+		delete wrongMeta;
+		delete wrongJ;
+	}
 
-	const WrongAnimal* wrongMeta = new WrongAnimal();
-	const WrongAnimal* wrongJ = new WrongCat();
-
-	std::cout << std::endl;
-
-	std::cout << wrongJ->getType() << " " << std::endl;	
-	wrongJ->makeSound();
-	wrongMeta->makeSound();
-
-	std::cout << std::endl;
-
-	delete wrongMeta;
-	delete wrongJ;
-	
+	leaks();
 	return 0; 
 }
