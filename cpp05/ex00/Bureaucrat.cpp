4c8aaa13ec2b1d5075a65821e8af58094b5f67af
Gichan Lee <gichanlee@icloud.com>
2023-12-22 18:00:14 +0900

    ex00

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -22,12 +22,80 @@ Bureaucrat::Bureaucrat() : _name("default"), _grade(150)
 {
 }
 
-Bureaucrat::Bureaucrat(std::string name, int grade) 
-	: _name(name), _grade(grade)
-	{
-		if (grade < 1)
-			throw Bureaucrat::GradeTooHighException();
-		else if (grade > 150)
-			throw Bureaucrat::GradeTooLowException();
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name), _grade(grade)
+{
+	try {
+		checkGrade(grade);
 	}
+	catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << e.what() << std::endl;
+		_grade = 1;
+	}
+	catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << e.what() << std::endl;
+		_grade = 150;
+	}
+}
 
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy) : _name(copy._name), _grade(copy._grade)
+{
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy)
+{
+	if (this == &copy)
+		return (*this);
+	_grade = copy._grade;
+	return (*this);
+}
+
+void Bureaucrat::checkGrade(int grade)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+const std::string &Bureaucrat::getName() const
+{
+	return (_name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (_grade);
+}
+
+void Bureaucrat::incrementGrade()
+{
+	try {
+		checkGrade(_grade - 1);
+		_grade--;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+}
+
+void Bureaucrat::decrementGrade()
+{
+	try {
+		checkGrade(_grade + 1);
+		_grade++;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+}
+
+std::ostream	&operator<<(std::ostream &out, const Bureaucrat &bureaucrat)
+{
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
+	return (out);
+}
---
+++ cpp05/ex00/main.cpp
@@ -0,0 +1,46 @@
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+	{
+		std::cin.get();
+		Bureaucrat b("b", 1);
+		std::cout << b << std::endl;
+		b.incrementGrade();
+		std::cout << b << std::endl;
+	}
+
+	{
+		std::cin.get();
+		Bureaucrat b("b", 150);
+		std::cout << b << std::endl;
+		b.incrementGrade();
+		std::cout << b << std::endl;
+	}
+
+	{
+		std::cin.get();
+		Bureaucrat b("b", 151);
+		std::cout << b << std::endl;
+		b.incrementGrade();
+		std::cout << b << std::endl;
+	}
+
+	{
+		std::cin.get();
+		Bureaucrat b("b", 0);
+		std::cout << b << std::endl;
+		b.incrementGrade();
+		std::cout << b << std::endl;
+	}
+
+	{
+		std::cin.get();
+		Bureaucrat b("b", 42);
+		std::cout << b << std::endl;
+		b.incrementGrade();
+		std::cout << b << std::endl;
+	}
+	
+	return (0);
+}
